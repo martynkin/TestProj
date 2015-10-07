0946538f63f50bca602eeaa77a11d4114b1fd5a3
martynkin <Alexey.Martynkin@smartbear.com>
2015-10-07 17:48:28 +0300

    e

--- src/Runner.java
+++ src/Runner.java
@@ -3,11 +3,5 @@ public class Runner {
 
 	public static void main(String[] args) {
 		System.out.println("testLine");
-<<<<<<< HEAD
-		//comment
-		//qw
-=======
-		//commit1
->>>>>>> branch 'master' of https://github.com/martynkin/TestProj.git
 	}
 }
