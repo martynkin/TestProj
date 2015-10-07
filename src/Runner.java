e8ecd0695db92ce33b1bc0865f813443238acbf4
martynkin <Alexey.Martynkin@smartbear.com>
2015-10-07 15:54:51 +0300

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -4,6 +4,7 @@ public class Runner {
 	public static void main(String[] args) {
 		//sdsdfdsfsdfsdfsdfsdfsdfsdfdsf
 		//erererrreerreerrererere
+		//////////////////////////////
 		System.out.println("testLine");
 	}
 
